fa17d2822cc5dccc34ac66715c5041bbaafcf151
Chatchai Jantaraprim <chatchai.j@gmail.com>
2023-02-20 10:49:31 +0700

    add test-send.sh

---
+++ test-send.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+URL="https://turtle.cjv6.net/~cj/github/Test1/webhook.php"
+
+data="{\"ref\": \"refname\"}"
+
+sig=$(	echo -n "${data}" |\
+		openssl dgst -sha1 -hmac "" |\
+		awk '{ print "X-Hub-Signature: sha1="$2}' )
+
+curl -X POST \
+	-H "Content-Type: application/json" \
+	-H "${sig}" \
+	-H "X-Github-Event: ping" \
+	--data "${data}" \
+	-s $URL
